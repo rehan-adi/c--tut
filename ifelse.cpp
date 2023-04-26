b74f794431176edeeaaaa3ba5d1cae16e2b0d4a9
rehan <rehanalire52@gmail.com>
2023-04-26 12:47:49 +0530

    hhh

---
+++ ifelse.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std ;
+
+int main (){
+
+int a ;
+cout<<"enter valu d of a"<<endl; 
+ cin>>a;
+  
+  for(int i=0;i<5;i++){
+    cout<<a<<endl;
+  }
+
+
+
+
+
+    return 0;
+}
